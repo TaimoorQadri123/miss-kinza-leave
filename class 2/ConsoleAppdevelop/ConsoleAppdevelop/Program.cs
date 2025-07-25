9238f5b9f81d44d18f28a6e55a51adf6a583cb20
TaimoorQadri123 <dchota374@gmail.com>
2025-07-25 15:18:59 +0500

    done

---
+++ class 2/ConsoleAppdevelop/ConsoleAppdevelop/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApphiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter your First Number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter your second Number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //int num1 = 10;
+            //int num2 = 20;
+            //Console.WriteLine($"Add: {num1} + {num2}  = {num1 + num2}");
+            //Console.WriteLine($"Sub: {num1} - {num2}  = {num1 - num2}");
+            //Console.WriteLine($"Mul: {num1} * {num2}  = {num1 * num2}");
+            //Console.WriteLine($"Div: {num1} / {num2}  = {num1 / num2}");
+            //Console.WriteLine($"Mod: {num1} % {num2}  = {num1 % num2}");
+
+
+            //Console.WriteLine("enter first number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter first number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"Is Equal : {num1 == num1 }");
+            //Console.WriteLine($"Is Not Equal : {num1 != num1 }");
+            //Console.WriteLine($"Is Greater : {num1 > num1  }");
+            //Console.WriteLine($"Is less : {num1 < num1  }");
+            //Console.WriteLine($"Is Greater or equal : {num1 >= num1 }");
+            //Console.WriteLine($"Is Less or equal : {num1 <= num1 }");
+
+
+            //string username = "Ali@gmai.com".ToLower();
+            //int password = 1234;
+
+            //if ((username == "ali" || username == "ali@gmail.com") & password == 1234) ;
+            //{
+            //    Console.WriteLine("Login successful");
+
+            //}
+            //else {
+            //    Console.WriteLine("Invalid username or password ");
+
+            //}
+
+            //int a = 13;
+            //Console.WriteLine(a++);
+            //Console.WriteLine(a);
+
+            int a = 13;
+            Console.WriteLine(a--);
+            Console.WriteLine(a);
+
+
+        }
+    }
+}
