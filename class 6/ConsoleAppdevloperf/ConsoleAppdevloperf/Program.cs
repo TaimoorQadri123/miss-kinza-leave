0e0619793ed527ba3438033732253b6795eb957c
TaimoorQadri123 <dchota374@gmail.com>
2025-08-01 23:32:40 +0500

    class 6

---
+++ class 6/ConsoleAppdevloperf/ConsoleAppdevloperf/Program.cs
@@ -0,0 +1,264 @@
+﻿namespace ConsoleAppdevloperf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i <=10; i++)
+            //{ 
+            //Console.WriteLine(i);
+            //}
+
+            //for (int i = 10; i >= 1; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i =0; i <= 20  ; i++)
+            //{
+            //    Console.WriteLine(i++ );
+            //}
+            //for (int i = 0; i <= 20; i+=2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 1; i <= 20; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //for (int i = 19; i >= 1; i -= 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //Console.WriteLine("Enter your table Number ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= 10; i ++)
+            //{
+
+
+            //    Console.WriteLine($"Table: {num} x {i} = {i * num}");
+
+            //}
+
+            //int i = 1;
+            //while (i<=10)
+            //{ 
+            //Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //int i = 10;
+            //while (i >= 1)
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+            //string correctusername ="Admin".ToLower();
+            //string correctpassword = "1234";
+
+            //string username, password;
+
+            //while (true) 
+
+            //{
+            //    Console.WriteLine("Enter your username ");
+            //    username = Console.ReadLine();
+            //    Console.WriteLine("Enter your password ");
+            //    password = Console.ReadLine();
+
+
+            //    if (username == correctusername && password == correctpassword) 
+
+            //    {
+            //    Console.WriteLine("Login Successful");
+            //        break;
+            //    }
+            //    else 
+            //    {
+            //        Console.WriteLine("Invalid credentials");
+            //    }
+
+
+            //}
+
+
+
+            //Console.WriteLine("Enter any word");
+            //string word = Console.ReadLine();
+
+
+            //while (i = word) ;
+            //{
+            //    Console.WriteLine(i);
+            //    i--;
+            //}
+
+
+            //Console.WriteLine("Enter your First Number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Enter 1 to 5 ");
+            //string opreator = Console.ReadLine();
+
+            //Console.WriteLine("Enter your second Number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //switch (opreator)
+            //{
+
+            //    case "1":
+            //        Console.WriteLine(num1 + num2);
+            //        break;
+
+            //    case "2":
+            //        Console.WriteLine(num1 - num2);
+            //        break;
+
+            //    case "3":
+            //        Console.WriteLine(num1 * num2);
+            //        break;
+
+            //    case "4":
+
+            //        if (num2 == 0)
+            //        {
+            //            Console.WriteLine("invalid value");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine(num1 / num2);
+            //        }
+
+
+            //        break;
+
+
+
+            //    case "5":
+            //        Console.WriteLine(num1 % num2);
+            //        break;
+
+
+
+            //    default:
+            //        Console.WriteLine("invalid Operator");
+            //        break;
+
+
+            //while (true)
+
+            //{
+
+            //    Console.WriteLine("Choose Operation:");
+            //    Console.WriteLine("1 - Addition");
+            //    Console.WriteLine("2 - Subtraction");
+            //    Console.WriteLine("3 - Multiplication");
+            //    Console.WriteLine("4 - Division");
+            //    Console.WriteLine("5 - Modulus");
+            //    Console.WriteLine("0 - Exit");
+            //    string select = Console.ReadLine();
+
+            //    if (select == "0")
+            //    {
+            //        Console.WriteLine("Exiting the calculator. Goodbye!");
+            //        break;
+            //    }
+
+            //    Console.WriteLine("Enter your First Number");
+            //    int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Enter your second Number");
+            //    int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+            //    if (select == "1")
+            //    {
+            //        Console.WriteLine("Result: " + (num1 + num2));
+            //    }
+            //    else if (select == "2")
+            //    {
+            //        Console.WriteLine("Result: " + (num1 - num2));
+            //    }
+            //    else if (select == "3")
+            //    {
+            //        Console.WriteLine("Result: " + (num1 * num2));
+            //    }
+            //    else if (select == "4")
+            //    {
+            //        if (num2 == 0)
+            //        {
+            //            Console.WriteLine("Error: Division by zero not allowed");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Result: " + (num1 / num2));
+            //        }
+            //    }
+            //    else if (select == "5")
+            //    {
+            //        Console.WriteLine("Result: " + (num1 % num2));
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Invalid Operation. Please choose between 1-5 or 0 to exit.");
+            //    }
+            //    Console.WriteLine("");
+
+            //    }
+
+            //int a = 10;
+            //do
+            //{
+            //    Console.WriteLine(a);
+            //    a++;
+            //} while (a<8);
+
+
+            //for (int i = 0; i <= 20; i++)
+            //{
+            //    Console.WriteLine(i++);
+            //}
+
+            //int a = 0;
+            //do
+            //{
+            //    Console.WriteLine(a++);
+            //    a++;
+
+            //} while (a <= 20);
+
+            int[] ages = new int[5];
+            ages[0] = 10;
+            ages[1] = 20;
+            ages[2] = 30;
+            ages[3] = 40;
+            ages[4] = 50;
+
+            foreach (int age in ages) 
+            
+            {
+            Console.WriteLine(age);
+
+            }
+
+
+
+            string[] names = { "ali", "asgar", "fahad " };
+
+            foreach (string name in names) 
+            {
+            Console.WriteLine($"{name}.{name}");
+            }
+        }
+    }
+}
